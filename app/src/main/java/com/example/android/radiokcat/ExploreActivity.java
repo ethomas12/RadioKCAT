399235f13683143a730a293614f029da02d96113
Elaine Thomas <el41net@gmail.com>
2018-06-13 23:27:57 -0600

    initial commit

---
+++ app/src/main/java/com/example/android/radiokcat/ExploreActivity.java
@@ -0,0 +1,37 @@
+package com.example.android.radiokcat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ExploreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.song_list);
+
+//        ArrayList<FamilyMember> familyMembers = new ArrayList<FamilyMember>();
+//
+//        familyMembers.add(new FamilyMember("father", "әpә"));
+//        familyMembers.add(new FamilyMember("mother", "әṭa"));
+//        familyMembers.add(new FamilyMember("son", "angsi"));
+//        familyMembers.add(new FamilyMember("daughter", "tune"));
+//        familyMembers.add(new FamilyMember("older brother", "taachi"));
+//        familyMembers.add(new FamilyMember("younger brother", "chalitti"));
+//        familyMembers.add(new FamilyMember("older sister", "teṭe"));
+//        familyMembers.add(new FamilyMember("younger sister", "kolliti"));
+//        familyMembers.add(new FamilyMember("grandmother", "ama"));
+//        familyMembers.add(new FamilyMember("grandfather", "paapa"));
+//
+//
+//        FamilyMemberAdapter adapter = new FamilyMemberAdapter(this, familyMembers);
+//
+//        ListView listView = (ListView) findViewById(R.id.list);
+//
+//        listView.setAdapter(adapter);
+    }
+}
+
